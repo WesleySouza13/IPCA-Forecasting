cb22fc01d3113102d02939093f47d914816dfc71
WesleySouza13 <souzabusiness13@gmail.com>
2025-12-02 09:14:12 -0300

    usando a classe DataEng para download e carregamento dos dados

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
 from src.DataEng.DataEng import DataEng
-url = 'https://api.bcb.gov.br/dados/serie/bcdata.sgs.433/dados?formato=json&dataInicial=01/01/2000&dataFinal=01/01/2025'
 
 obj = DataEng()
-data_inicio = '01/01/2000'
+data_inicio = '01/01/2000' #a data que eu quero para o inicio do estudo 
 obj.load(code=433, start_date=data_inicio)
-obj.feature_eng()
-obj.to_parquet(name='IPCA')
+obj.feature_eng() # faz a transformaçao de colunas 
+obj.to_parquet(name='IPCA') # exporta para parquet 
